8cff19d9b8aaed1cce7cb89d150b7423c7b6af1f
Wyatt Johnson <61155572+wyattjohnsonbro@users.noreply.github.com>
2026-04-06 18:21:33 -0400

    Add back breaks restriction cuz the pizzeria wants to be difficult ig

--- a.lua
+++ a.lua
@@ -2540,7 +2540,7 @@ ESPBox:AddToggle("DoorHits", {
                 local breaks = v:GetAttribute("Breaks")
                 if breaks == nil then return end
 
-                -- if breaks > 3 then return end
+                if breaks > 3 then return end
 
                 local gui = addSimpleTextLabel(
                     v,
